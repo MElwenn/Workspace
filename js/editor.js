a6d27c3007ed9249a21662dc4f2d57522ff890ec
Florian Fladung <mail@ffladung.com>
2020-12-27 19:14:34 +0100

    desk and wall editor selection

--- js/editor.js
+++ js/editor.js
@@ -42,34 +42,99 @@ var isPaint = false;
 var mode = 'brush';
 var lastLine;
 
-office.on('mousedown touchstart', function (e) {
-    isPaint = true;
-    var pos = office.getPointerPosition();
-    lastLine = new Konva.Line({
-      stroke: '#df4b26',
-      strokeWidth: 5,
-      globalCompositeOperation:
-        mode === 'brush' ? 'source-over' : 'destination-out',
-      points: [pos.x, pos.y, pos.x, pos.y],
+function activateWallTool() {
+    office.on('mousedown touchstart', function (e) {
+        isPaint = true;
+        var pos = office.getPointerPosition();
+        lastLine = new Konva.Line({
+          stroke: '#df4b26',
+          strokeWidth: 5,
+          globalCompositeOperation:
+            mode === 'brush' ? 'source-over' : 'destination-out',
+          points: [pos.x, pos.y, pos.x, pos.y],
+        });
+        wallsLayer.add(lastLine);
     });
-    wallsLayer.add(lastLine);
-  });
+    
+    office.on('mouseup touchend', function () {
+        isPaint = false;
+    });
+    
+    // and core function - drawing
+    office.on('mousemove touchmove', function () {
+        if (!isPaint) {
+            return;
+        }
+    
+        const pos = office.getPointerPosition();
+        var newPoints = lastLine.points();
+        newPoints[2] = pos.x;
+        newPoints[3] = pos.y;
+        lastLine.points(newPoints);
+        wallsLayer.batchDraw();
+    });
+}
 
-office.on('mouseup touchend', function () {
-    isPaint = false;
-});
+function deactivateWallTool() {
+    office.off('mousedown touchstart');
+    office.off('mouseup touchend');
+    office.off('mousemove touchmove');
+}
 
-// and core function - drawing
-office.on('mousemove touchmove', function () {
-    if (!isPaint) {
-        return;
-    }
+// DESKS
 
-    const pos = office.getPointerPosition();
-    var newPoints = lastLine.points();
-    newPoints[2] = pos.x;
-    newPoints[3] = pos.y;
-    lastLine.points(newPoints);
-    wallsLayer.batchDraw();
-});
+var deskLayer = new Konva.Layer();
+office.add(deskLayer);
+
+function activateDeskTool() {
+    office.on('mousedown touchstart', function (e) {
+        var pos = office.getPointerPosition();
+        var desk = createDesk();
+
+        desk.x(pos.x);
+        desk.y(pos.y);
+
+        deskLayer.add(desk);
+        deskLayer.draw();
+    });
+}
+
+function createDesk() {
+    return new Konva.Rect({
+        width: 2 * gridSize,
+        height: gridSize,
+        fill: 'grey',
+        stroke: 'black',
+        strokeWidth: 4,
+        cornerRadius: 4,
+    });
+}
+
+function deactivateDeskTool() {
+    office.off('mousedown touchstart');
+}
+
+var wallToolRadio = document.getElementById('tool-wall');
+var deskToolRadio = document.getElementById('tool-desk');
+
+wallToolRadio.onchange = function() {
+    deactivateDeskTool();
+    activateWallTool();
+}
+
+deskToolRadio.onchange = function() {
+    deactivateWallTool();
+    activateDeskTool();
+}
+
+if (wallToolRadio.checked) {
+    wallToolRadio.onchange();
+}
+
+if (deskToolRadio.checked) {
+    deskToolRadio.onchange();
+}
+
+
+
 
