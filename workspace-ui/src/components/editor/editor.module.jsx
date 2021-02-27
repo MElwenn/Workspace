2d75b82d28b1b317c878e2ca71c177d5fbb8f7cc
Florian Fladung <mail@ffladung.com>
2021-02-27 19:24:26 +0100

    import image from editor into office background - WIP

--- workspace-ui/src/components/editor/editor.module.jsx
+++ workspace-ui/src/components/editor/editor.module.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import Office from "../office/office.module";
 
 import styles from "./editor.module.css"
 
-const Editor = () => {
-  return (
-    <div>
-      THE Editor
+class Editor extends Component {
 
-      <Office />
-    </div>
-  );
+  state = {
+    imageUrl: "",
+    imageUrlInputValue: ""
+  }
+
+  handleInput = event => {
+    this.setState({ imageUrlInputValue: event.target.value });
+  };
+
+  updateImage = () => {
+    // this.setState(Object.create(this.state, {imageUrl: this.state.imageUrlInputValue}))
+    this.setState({ imageUrl: this.state.imageUrlInputValue });
+  }
+
+  render() {
+    return (
+      <div id="editor">
+
+        <div id={styles.uploadForm}>
+          <h4>Please insert image url to import</h4>
+          <input id="image-url-input" type="text" size="50" onChange={this.handleInput} />
+          <br />
+          <button onClick={this.updateImage}>OK</button>
+        </div>
+        <Office layoutImage={this.state.imageUrl} />
+      </div>
+    );
+  }
 };
 
 export default Editor;
--- workspace-ui/src/components/office/office.module.jsx
+++ workspace-ui/src/components/office/office.module.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
 import styles from "./office.module.css"
 
-const Office = () => {
-  return (
-    <div>THE OFFICE</div>
-  );
+class Office extends Component {
+
+  constructor(props) {
+    super(props)
+    // TODO pass this value properly and trigger re-render of component
+    this.state = { layoutImage: props.layoutImage };
+  }
+
+  importLayout = () => {
+    return {
+      fontWeight: "bold",
+      color: "blue",
+      backgroundImage: `url("${this.state.layoutImage}")`
+    }
+  }
+
+  render() {
+    return (
+      <div style={this.importLayout()}>THE OFFICE</div>
+    );
+  }
 };
 
 export default Office;
